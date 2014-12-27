b9ccecbba3eb8a464517f0893f2180dab1aa4609
DolphinDev <miguel.dxm@gmail.com>
2014-12-27 14:25:08 +0000

    Create Arena.java

---
+++ MinigameFactory/com/darkzonex44/html/arena/api/Arena.java
@@ -0,0 +1,27 @@
+package com.darkzonex44.html.arena.api;
+
+
+
+public class Arena {
+private String name;
+private boolean started;
+private String author;
+public ArrayList<Arena> objects;
+private int max;
+private int min;
+private String game;
+private boolean full;
+
+
+/** ^ Not Sorted Yet **/
+
+
+
+
+
+/** Constructor **/
+
+
+/** Methods **/
+
+}
